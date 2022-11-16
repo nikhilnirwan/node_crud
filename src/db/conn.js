2d802d3a001fe5acc2513450ff84c3403c10a148
nikhil <nikhil.nirwan@startlazaa.com>
2022-11-16 14:05:33 +0530

    add

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-	.connect('mongodb://localhost:27017/olymics')
-	.then(() => {
-		console.log('connection successful');
-	})
-	.catch((e) => {
-		console.log('No connection');
-	});
+  .connect("mongodb://localhost:27017/olymics") //
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((e) => {
+    console.log("No connection");
+  });
